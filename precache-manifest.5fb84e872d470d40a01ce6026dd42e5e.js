cf071360df81df3e3264476fc524ab490fab13e0
Code Sandbox <hello@codesandbox.io>
2021-10-16 03:47:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5fb84e872d470d40a01ce6026dd42e5e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a62b91b792932a906a4cdfa6eaca075",
+    "url": "https://agomez99.github.io/csb-o2216/index.html"
+  },
+  {
+    "revision": "49ebc16f9d6e50840ac6",
+    "url": "https://agomez99.github.io/csb-o2216/static/css/main.a056ab23.chunk.css"
+  },
+  {
+    "revision": "560ccf815f0e956c3757",
+    "url": "https://agomez99.github.io/csb-o2216/static/js/2.76d4f2fd.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://agomez99.github.io/csb-o2216/static/js/2.76d4f2fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ebc16f9d6e50840ac6",
+    "url": "https://agomez99.github.io/csb-o2216/static/js/main.91e1e30b.chunk.js"
+  },
+  {
+    "revision": "21b4d7e8201fd91c138e",
+    "url": "https://agomez99.github.io/csb-o2216/static/js/runtime-main.035bda78.js"
+  },
+  {
+    "revision": "a1913f883278367eb7912b9f514181b4",
+    "url": "https://agomez99.github.io/csb-o2216/static/media/50-tobylane.a1913f88.mp3"
+  },
+  {
+    "revision": "6a28325ff041f8213d4f77ae0ad2be47",
+    "url": "https://agomez99.github.io/csb-o2216/static/media/cali-wataboi.6a28325f.mp3"
+  },
+  {
+    "revision": "01f6eb99187bc00c44214e6116673a99",
+    "url": "https://agomez99.github.io/csb-o2216/static/media/iwonder-dreamheaven.01f6eb99.mp3"
+  },
+  {
+    "revision": "60585ee7bcd43b827e5be952828ebd79",
+    "url": "https://agomez99.github.io/csb-o2216/static/media/next.60585ee7.svg"
+  },
+  {
+    "revision": "28d702d39257e9eee89945faf4ae275d",
+    "url": "https://agomez99.github.io/csb-o2216/static/media/pause.28d702d3.svg"
+  },
+  {
+    "revision": "359e87c77bdaffacb3b0c03e763f0bfb",
+    "url": "https://agomez99.github.io/csb-o2216/static/media/play.359e87c7.svg"
+  },
+  {
+    "revision": "a5790adb0648ac5a1b0ae88e6270dd13",
+    "url": "https://agomez99.github.io/csb-o2216/static/media/prev.a5790adb.svg"
+  }
+]);
